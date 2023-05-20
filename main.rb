fc84f6634d0d4b6b995ae3a0ce38360b43299a53
matterramun <Veradux4@protonmail.com>
2023-05-19 21:17:23 -0400

    Add initialize, show board, and play methods
    Starting by building everything into main
    - Create initialize with globals for guesses, guess results, and the code
    - Add method to show the full list of guesses and guess results
    - Add play method to generate a random code and accept guesses to be added to guess list.

--- main.rb
+++ main.rb
@@ -1,8 +1,40 @@
 # frozen_string_literal: true
 
-require_relative '/lib/board'
-require_relative '/lib/player'
+#require_relative '/lib/board'
+#require_relative '/lib/player'
 
-def play
+class Mastermind
+  def initialize
+    @board = []
+    @guess = []
+    @code = nil
+    play
+    showboard
+  end
+
+  def showboard
+    @board.each_with_index do |code, _code_index|
+      print @guess[_code_index]
+      print '|'
+      puts code
+    end
+  end
+
+  def play
+    puts 'Generating code...'
+    @code = rand(1000..9999)
+    puts @code
+    (i=12).times do |i|
+        if i > 0
+          puts "What is your guess?"
+          @guess << gets.chomp.to_i
+          showboard
+        #elsif
+
+        end
+    end
+  end
 
 end
+
+Mastermind.new

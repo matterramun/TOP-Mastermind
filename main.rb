38da7bebac4d3c3a277910e075d2b943f489aaeb
matterramun <Veradux4@protonmail.com>
2023-05-21 15:53:37 -0400

    Reversed loop parentage. Loop code as parent
    Results with making the parent loop through code then guess is so much better!
    Getting accurate number of guess attempt array entries, most of the time.
    Full miss results are accurate now.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-#require_relative '/lib/board'
-#require_relative '/lib/player'
+# require_relative '/lib/board'
+# require_relative '/lib/player'
 
 class Mastermind
   def initialize
@@ -9,32 +9,51 @@ class Mastermind
     @guess = []
     @code = nil
     play
-    showboard
-  end
-
-  def showboard
-    @board.each_with_index do |code, _code_index|
-      print @guess[_code_index]
-      print '|'
-      puts code
-    end
+    #showboard
   end
 
   def play
     puts 'Generating code...'
     @code = rand(1000..9999)
     puts @code
-    (i=12).times do |i|
-        if i > 0
-          puts "What is your guess?"
-          @guess << gets.chomp.to_i
-          showboard
-        #elsif
-
-        end
+    (i = 12).times do
+      if i.positive?
+        guessattempt
+        showboard
+      end
     end
   end
 
+  def showboard
+    @board.each_with_index do |code, code_index|
+      print @guess[code_index]
+      print '|'
+      puts code
+    end
+  end
+
+  def guessattempt
+    puts 'What is your guess?'
+    guess_temp = gets.chomp.to_i
+    @guess << guess_temp
+    guess_array = guess_temp.digits
+    code_array = @code.digits
+    guess_result = []
+    code_array.each_with_index do |digit, digit_index| # Iterates through each digit to eval match, mismatch, or miss
+      guess_array.each_with_index do |code_digit, code_digit_index|
+        if digit == code_digit && digit_index == code_digit_index
+          guess_result << digit
+          next
+        elsif digit == code_digit
+          guess_result << '?'
+          next
+        end
+        guess_result << 'x' if guess_result.length() == digit_index 
+        puts "Guess iteration: #{guess_result}"
+      end
+    end
+    @board << guess_result.join() #This doesnt work...
+  end
 end
 
 Mastermind.new

3cf32dc3053b53456507f6fde4b01a8ac063137c
matterramun <Veradux4@protonmail.com>
2023-05-25 17:10:28 -0400

    Remove Console Logging, add clears to clean up the terminal during play!

--- main.rb
+++ main.rb
@@ -12,13 +12,14 @@ class Mastermind
   end
 
   def play
-    puts 'Generating code...'
+    # puts 'Generating code...'
     @code = rand(1000..9999)
-    puts @code
+    # puts @code
     (i = 12).times do
       if i.positive?
-        guessattempt
+        system "clear"
         showboard
+        guessattempt
       end
     end
     puts "You're out of guesses!"
@@ -55,7 +56,7 @@ class Mastermind
     # Iterates through each digit to eval match, mismatch, or miss
     guess_array.each_with_index do |digit, digit_index|
       code_array.each_with_index do |code_digit, code_digit_index|
-        puts "Code: #{code_digit} Guess: #{digit}. Index code: #{code_digit_index} Index guess: #{digit_index}" # Console Logging
+        # puts "Code: #{code_digit} Guess: #{digit}. Index code: #{code_digit_index} Index guess: #{digit_index}" # Console Logging
         if digit == code_digit && digit_index == code_digit_index
           guess_result << digit
           break
@@ -65,11 +66,11 @@ class Mastermind
            break
           elsif guess_array[code_digit_index] == digit && code_digit_index > digit_index # there is a true match with the same digit on a higher index, return x
             guess_result << 'x'
-            puts "Future true match"
+            # puts "Future true match"
           elsif guess_array.select{|e| e == digit}.length >= code_array.select{|e| e == digit}.length && 
               code_array.select{|e| e == digit} == guess_array.select.with_index{|e,i| e == digit && (guess_result[i] == '?' || guess_result[i] == digit)} #There is already a mismatch for this digit and the number of instances of this digit is met
             guess_result << 'x'
-            puts "Reducing number of mismatches to equal to results in the code"
+            # puts "Reducing number of mismatches to equal to results in the code"
           else 
             guess_result << '?'
           end
@@ -77,7 +78,7 @@ class Mastermind
         end
       end
       guess_result << 'x' if guess_result.length == digit_index
-      puts "Guess iteration: #{guess_result}" # Console Logging
+      # puts "Guess iteration: #{guess_result}" # Console Logging
     end
     @board << guess_result.join
     victory_check(guess_array,code_array)
@@ -85,6 +86,7 @@ class Mastermind
 
   def victory_check(guess_array,code_array)
     if guess_array == code_array
+      system "clear"
       showboard
       puts "You Win!"
       exit

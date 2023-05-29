afdbfbe20614c9f9e561426489d426721bf57a82
matterramun <Veradux4@protonmail.com>
2023-05-29 18:54:40 -0400

    Add @player validation, PC initialization, and Utils.
    - Ensure that player selection is either 1 or 2
    - Added initialization of persistent PC arrays
    - Added several utils for capturing digit and location of perfect and imperfect matches to PC arrays for validating guesses and facilitating future guesses.

--- main.rb
+++ main.rb
@@ -16,9 +16,17 @@ class Mastermind
     loop do
       puts 'Who is the Player? 1 for You, 2 for the PC'
       @player = gets.chomp.to_i
-      if @player != 1 || @player != 2 
+      puts @player
+      if @player.nil?
+        puts "Pick 1 or 2"
+      elsif @player < 1
+        puts "Pick 1 or 2"
+      elsif @player > 2
+        puts "Pick 1 or 2"
+      else
         break
-      end
+    end
+      pc_utils(0) if @player == 2
     end
   end
 
@@ -40,14 +48,15 @@ class Mastermind
           break
         end
       end
+      pc_utils(0,"") if @player == 2
     end
-    #// puts @code
+    # // puts @code
     (i = 12).times do
-      if i.positive?
-        system "clear"
-        showboard
-        guessattempt
-      end
+      next unless i.positive?
+
+      # system "clear"
+      showboard
+      guessattempt
     end
     puts "You're out of guesses!"
     exit
@@ -87,37 +96,50 @@ class Mastermind
     # Iterates through each digit to eval match, mismatch, or miss
     guess_array.each_with_index do |digit, digit_index|
       code_array.each_with_index do |code_digit, code_digit_index|
-        #// puts "Code: #{code_digit} Guess: #{digit}. Index code: #{code_digit_index} Index guess: #{digit_index}" # Console Logging
+        # // puts "Code: #{code_digit} Guess: #{digit}. Index code: #{code_digit_index} Index guess: #{digit_index}" # Console Logging
+        digit_array = [digit, digit_index] if @player == 2
+        # // puts "digit_array: #{digit_array}"
         if digit == code_digit && digit_index == code_digit_index
           guess_result << digit
+          pc_utils(1, digit_array) if @player == 2
           break
         elsif digit == code_digit && digit_index != code_digit_index
           if guess_array[digit_index] == code_array[digit_index] # ensure matching index doesnt have a match before declaring mismatch
-           guess_result << digit
-           break
+            guess_result << digit
+            pc_utils(1, digit_array) if @player == 2
+            break
           elsif guess_array[code_digit_index] == digit && code_digit_index > digit_index # there is a true match with the same digit on a higher index, return x
             guess_result << 'x'
-            #// puts "Future true match"
-          elsif guess_array.select{|e| e == digit}.length >= code_array.select{|e| e == digit}.length && 
-              code_array.select{|e| e == digit} == guess_array.select.with_index{|e,i| e == digit && (guess_result[i] == '?' || guess_result[i] == digit)} #There is already a mismatch for this digit and the number of instances of this digit is met
+            # // puts "Future true match"
+          elsif guess_array.select { |e| e == digit }.length >= code_array.select { |e| e == digit }.length &&
+                # There is already a mismatch for this digit and the number of instances of this digit is met
+                code_array.select do |e|
+                  e == digit
+                end == guess_array.select.with_index do |e, i|
+                         e == digit && (guess_result[i] == '?' || guess_result[i] == digit)
+                       end
+
             guess_result << 'x'
-            #// puts "Reducing number of mismatches to equal to results in the code"
-          else 
+            # // puts "Reducing number of mismatches to equal to results in the code"
+          else
             guess_result << '?'
+            pc_utils(2, digit_array) if @player == 2
           end
           break
         end
       end
       guess_result << 'x' if guess_result.length == digit_index
-      #// puts "Guess iteration: #{guess_result}" # Console Logging
+      # // puts "Guess iteration: #{guess_result}" # Console Logging
     end
     @board << guess_result.join
-    victory_check(guess_array,code_array)
+    victory_check(guess_array, code_array)
   end
 
   def pc_guesser
+    puts "match_array: #{@match_array}"
+    puts "imperfect_array: #{@imperfect_array}"
     case @board.length
-    when 0 
+    when 0
       1122
     when 1
       3456
@@ -127,21 +149,19 @@ class Mastermind
       # placeholder until the guessing logic is implemented
       rand(1000..9999)
     else
-      match_array = []
-      imperfect_array = []
       imperfect_array_index = []
-      @board.each_with_index do |guess, guess_index|
+      @board.each_with_index do |guess, _guess_index|
         match_array << guess.scan(/[0-9]/)
-        puts "match_array: #{match_array}"
+
         # Get any mismatches, digit from @board at guess_index that match ? in @guess
-        guess_array= guess.chars
-        imperfect_array_index << guess_array.each_index.select{|i| guess_array[i] == '?'}.join.to_i
+        guess_array = guess.chars
+        imperfect_array_index << guess_array.each_index.select { |i| guess_array[i] == '?' }.join.to_i
         # Match to @board entry digit and flatten
         puts "imperfect_array_index: #{imperfect_array_index}"
         imperfect_array_index.each_with_index do |board_index, digit_index|
           guess_chars = @guess[digit_index].digits.reverse
           puts "guess_chars: #{guess_chars}"
-          #guess_chars = guess_chars.chars
+          # guess_chars = guess_chars.chars
           imperfect_array << guess_chars[board_index]
         end
         puts "imperfect_array: #{imperfect_array}"
@@ -149,13 +169,26 @@ class Mastermind
     end
   end
 
-  def victory_check(guess_array,code_array)
-    if guess_array == code_array
-      system "clear"
-      showboard
-      puts "You Win!"
-      exit
+  def pc_utils(tool, input)
+    puts "pc_utils: tool #{tool} input #{input}"
+    case tool
+    when 0
+      @match_array = []
+      @imperfect_array = []
+    when 1 # Perfect match submission
+      @match_array << input
+    when 2 # Mismatch submission
+      @imperfect_array << input
     end
+  end
+
+  def victory_check(guess_array, code_array)
+    return unless guess_array == code_array
+
+    # system "clear"
+    showboard
+    puts 'You Win!'
+    exit
   end
 end
 
